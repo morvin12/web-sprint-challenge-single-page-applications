46fc55c9511c9fa88a5fc1e9df56fe2597b0d931
Michael Orvin <morvin1212@gmail.com>
2021-11-14 11:41:51 -0700

    Set up path for Homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { link, Switch, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <>
+    <div className="navigation">
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+      <nav>
+        <link to="/">Home</link>
+        <link to="/pizza">Order Pizza</link>
+        <link to="/completion">Completion</link>
+      </nav>
+    </div>
+
+    <Switch>
+      <Route path="/pizza">
+        <PizzaForm
+          values={formValues}
+          change={inputChange}
+          submit={formsubmit}
+          disabled={disabled}
+          errors={formerros}
+        />
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
+      <Route path="/order">
+        <Order details={orders} />
+      </Route>
+    </Switch>
   );
 };
 export default App;
