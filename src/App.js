e93aa7c417855cbc00204c8ae7a1da166a31eecf
Michael Orvin <morvin1212@gmail.com>
2021-11-14 12:35:56 -0700

    Finished Pizza Form

--- src/App.js
+++ src/App.js
@@ -3,23 +3,24 @@ import { link, Switch, Route } from 'react-router-dom';
 
 const App = () => {
   return (
+  <div>
     <div className="navigation">
       <h1>Lambda Eats</h1>
       <nav>
-        <link to="/">Home</link>
-        <link to="/pizza">Order Pizza</link>
-        <link to="/completion">Completion</link>
+      <link to="/">Home</link>
+      <link to="/pizza">Order Pizza</link>
+      <link to="/completion">Completion</link>
       </nav>
     </div>
 
     <Switch>
       <Route path="/pizza">
         <PizzaForm
-          values={formValues}
-          change={inputChange}
-          submit={formsubmit}
-          disabled={disabled}
-          errors={formerros}
+        values={formValues}
+        change={inputChange}
+        submit={formSubmit}
+        disabled={disabled}
+        errors={formErrors}
         />
       </Route>
       <Route path="/">
@@ -29,6 +30,8 @@ const App = () => {
         <Order details={orders} />
       </Route>
     </Switch>
+  </div>
+
   );
 };
 export default App;
---
+++ src/Components/PizzaForm.js
@@ -0,0 +1,121 @@
+import React from "react";
+
+const PizzaForm = (props) => {
+    const {
+        change,
+        values,
+        submit,
+        errors,
+    } = props
+
+    const onSubmit = event => {
+        event.preventDefault()
+        submit()
+    }
+
+    const onChange = event => {
+        const {name, value, checked, type} = event.target
+        const realValue = type === 'checkbox' ? checked : value;
+        change(name, realValue)
+    }
+
+    return (
+        <div className="formContainer">
+            <div className="formHeader">
+                <h2>Start Building</h2>
+            </div>
+            <form id="pizza-form" onSubmit={onSubmit}>
+                <label>Name For The Order
+                    <input
+                        value={values.name}
+                        name="name"
+                        id="name-input"
+                        type="text"
+                    />
+                </label>
+                <label>Select Your Size
+                    <select
+                        value={values.size}
+                        id="size-dropdown"
+                        onChange={onChange}
+                        name="size"
+                    >
+                        <option value="">---Size---</option>
+                        <option value="small">Small</option>
+                        <option value="medium">Medium</option>
+                        <option value="Large">Large</option>
+                    </select>
+                </label>
+                <label>Marinara
+                    <input
+                        type='radio'
+                        name='sauce'
+                        value='marinara'
+                        onChange={onChange}
+                        checked={values.sauce === 'marinara'} 
+                    />
+                </label>
+                <label>Alfredo
+                    <input
+                        type='radio'
+                        name='sauce'
+                        value='alfredo'
+                        onChange={onChange}
+                        checked={values.sauce === 'alfredo'} 
+                    />
+                </label>
+                <div className="topping">
+                    <h4>Select Toppings</h4>
+                <label>Pepperoni
+                    <input
+                        type='checkbox'
+                        name='pepperoni'
+                        onChange={onChange}
+                        checked={values.pepperoni}
+                    />
+                </label>
+                <label>Onions
+                    <input
+                        type='checkbox'
+                        name='onions'
+                        onChange={onChange}
+                        checked={values.onions}
+                    />
+                </label>
+                <label>Olives
+                    <input
+                        type='checkbox'
+                        name='olives'
+                        onChange={onChange}
+                        checked={values.olives}
+                    />
+                </label>
+                <label>Artichoke
+                    <input
+                        type='checkbox'
+                        name='artichoke'
+                        onChange={onChange}
+                        checked={values.artichoke}
+                    />
+                </label>
+                </div>
+                <label>Special Instructions
+                    <textarea
+                        id="special-text"
+                        name="special"
+                        type="text"
+                        maxLength="250"
+                        value={values.special}
+                        onChange={onChange}
+                    />
+                </label>
+                <div className="errors">
+                    <div>{errors.name}</div>
+                    <div>{errors.size}</div>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default PizzaForm;
